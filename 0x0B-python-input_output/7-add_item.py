608cfcb188ff06d2e46256190f23a5b881df8b61
KimFarida <farimomoh@gmail.com>
2022-08-02 15:15:12 +0000

    Correction of 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
-"""add_item
 """
+Module for saving argv info via json to file.
+"""
+import json
+import os.path
 import sys
-load_from_json_file = __import__('5-load_from_json_file').load_from_json_file
-save_to_json_file = __import__('6-save_to_json_file').save_to_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-try:
-    loadFile = load_from_json_file("add_item.json")
-except FileNotFoundError:
-    loadFile = []
+file = "add_item.json"
+json_list = []
+if os.path.exists(file):
+    json_list = load_from_json_file(file)
 
-argc = len(sys.argv)
-for idx in range(1, argc):
-    loadFile.append(sys.argv[idx])
-save_to_json_file(loadFile, "add_item.json")
+for i in range(1, len(sys.argv)):
+    json_list.append(sys.argv[i])
+
+save_to_json_file(json_list, file)
