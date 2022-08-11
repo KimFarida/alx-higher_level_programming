51237adf738f8389121be3676939b9a826d55680
KimFarida <farimomoh@gmail.com>
2022-08-11 04:02:45 -0700

    Correction

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
-'''Module for MyInt class.'''
-
-
 class MyInt(int):
-    def __eq__(self, other):
-        '''Override equals, inverting it.'''
-        return int(self) != int(other)
+    def __init__(self, value):
+        self.var = value
 
-    def __ne__(self, other):
-        '''Override not-equals, inverting it.'''
-        return int(self) == int(other)
+    def __eq__(self, var):
+        return self.var != int(var)
+
+    def __ne__(self, var):
+        return self.var == int(var)
