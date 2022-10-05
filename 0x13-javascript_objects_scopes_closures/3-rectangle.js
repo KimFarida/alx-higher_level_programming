302d798fdad7268958c476a8d0f9e7cc0e11666a
KimFarida <farimomoh@gmail.com>
2022-10-05 16:21:50 -0700

    Prints Rectangle to the console

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -4,4 +4,8 @@ module.exports = class Rectangle {
   constructor (w, h) {
     if (w && h > 0) [this.width, this.height] = [w, h];
   }
+
+  print () {
+    for (let row = 0; row < this.height; row++)console.log('X'.repeat());
+  }
 };
